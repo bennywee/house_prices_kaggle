ad5337b3f6146bc326a369f3371139cf9f07973c
occamsblazer3 <benw.cy93@gmail.com>
2020-07-26 13:10:07 +1000

    initiated repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description="Applying bayesian techniques learned from Statistical Rethinking and other resources in Kaggle's House Prices competition."",
+    author='Ben Wee',
+    license='',
+)
